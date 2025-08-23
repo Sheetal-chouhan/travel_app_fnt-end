fbbac6ed418c15e1afb304ffebaff070440e6d35
Sheetal Chouhan <sheetalchouhan@Sheetals-MacBook-Pro.local>
2025-08-23 22:20:21 +0530

    WIP: current changes

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import App from "./App";
-// import { CategoryProvider, DateProvider, FilterProvider, AuthProvider } from "./context";
-
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <Router>
-//       <CategoryProvider>
-//         <DateProvider>
-//           <FilterProvider>
-//             <AuthProvider>
-//               <App />
-//             </AuthProvider>
-
-
-//           </FilterProvider>
-
-//         </DateProvider>
-
-//       </CategoryProvider>
-//     </Router>
-
-
-//   </React.StrictMode>
-// );
-
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
