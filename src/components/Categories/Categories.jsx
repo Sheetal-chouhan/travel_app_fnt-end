e369ab9304d9a248520bb3a078dfe273badec1c3
Sheetal Chouhan <sheetalchouhan@Sheetals-MacBook-Pro.local>
2025-08-26 13:50:48 +0530

    added or update the  correction in file

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -150,93 +150,183 @@
 
 
 
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { useCategory, useFilter } from "../../context";
+
+// import "./Categories.css";
+
+// export const Categories = () => {
+//   const [categories, setCategories] = useState([]);
+//   const [numberOfCategoryToShow, setNumberOfCategoryToShow] = useState(0);
+//   const { hotelCategory, setHotelCategory } = useCategory();
+
+//   const { filterDispatch } = useFilter();
+
+//   const handleShowMoreRightClick = () => {
+//     setNumberOfCategoryToShow((prev) => prev + 10);
+//   };
+
+//   const handleShowMoreLeftClick = () => {
+//     setNumberOfCategoryToShow((prev) => prev - 10);
+//   };
+
+//   const handleFilterClick = () => {
+//     filterDispatch({
+//       type: "SHOW_FILTER_MODAL",
+//     });
+//   };
+
+//   useEffect(() => {
+//     (async () => {
+//       try {
+//         const { data } = await axios.get(
+//           "https://travel-app-vm2w.onrender.com/api/category"
+//         );
+//         const categoriesToShow = data.slice(
+//           numberOfCategoryToShow + 10 > data.length
+//             ? data.length - 10
+//             : numberOfCategoryToShow,
+//           numberOfCategoryToShow > data.length
+//             ? data.length
+//             : numberOfCategoryToShow + 10
+//         );
+//         setCategories(categoriesToShow);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     })();
+//   }, [numberOfCategoryToShow]);
+
+//   const handleCategoryClick = (category) => {
+//     setHotelCategory(category);
+//   };
+
+//   return (
+//     <section className="categories d-flex align-center gap-large cursor-pointer">
+//       {numberOfCategoryToShow >= 10 && (
+//         <button
+//           className="button btn-category btn-left fixed cursor-pointer"
+//           onClick={handleShowMoreLeftClick}
+//         >
+//           <span class="material-icons-outlined">chevron_left</span>
+//         </button>
+//       )}
+//       {categories &&
+//         categories.map(({ _id, category }) => (
+//           <span
+//             className={`${category === hotelCategory ? "border-bottom" : ""}`}
+//             key={_id}
+//             onClick={() => handleCategoryClick(category)}
+//           >
+//             {category}
+//           </span>
+//         ))}
+//       {numberOfCategoryToShow - 10 < categories.length && (
+//         <button
+//           className="button btn-category btn-right fixed cursor-pointer"
+//           onClick={handleShowMoreRightClick}
+//         >
+//           <span class="material-icons-outlined">chevron_right</span>
+//         </button>
+//       )}
+//       <button
+//         className="button btn-filter d-flex align-center gap-small cursor-pointer fixed"
+//         onClick={handleFilterClick}
+//       >
+//         <span className="material-icons-outlined">filter_alt</span>
+//         <span>Filter</span>
+//       </button>
+//     </section>
+//   );
+// };
+
+
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useCategory, useFilter } from "../../context";
+import { useCategory  , useFilter} from "../../context";
+
 
 import "./Categories.css";
 
+
 export const Categories = () => {
-  const [categories, setCategories] = useState([]);
-  const [numberOfCategoryToShow, setNumberOfCategoryToShow] = useState(0);
-  const { hotelCategory, setHotelCategory } = useCategory();
 
-  const { filterDispatch } = useFilter();
+    const [categories, setCategories] = useState([]);
 
-  const handleShowMoreRightClick = () => {
-    setNumberOfCategoryToShow((prev) => prev + 10);
-  };
+    const [numberOfCategoryToShow, setNumberOfCategoryToShow] = useState(0);
+    const { hotelCategory ,setHotelCategory} = useCategory();
 
-  const handleShowMoreLeftClick = () => {
-    setNumberOfCategoryToShow((prev) => prev - 10);
-  };
+    const { filterDispatch } = useFilter();
 
-  const handleFilterClick = () => {
-    filterDispatch({
-      type: "SHOW_FILTER_MODAL",
-    });
-  };
+    const handleShowMoreRightClick = () => {
+        setNumberOfCategoryToShow((prev) => prev + 10);
+    };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await axios.get(
-          "https://travel-app-vm2w.onrender.com/api/category"
-        );
-        const categoriesToShow = data.slice(
-          numberOfCategoryToShow + 10 > data.length
-            ? data.length - 10
-            : numberOfCategoryToShow,
-          numberOfCategoryToShow > data.length
-            ? data.length
-            : numberOfCategoryToShow + 10
-        );
-        setCategories(categoriesToShow);
-      } catch (err) {
-        console.log(err);
-      }
-    })();
-  }, [numberOfCategoryToShow]);
+    const handleShowMoreLeftClick = () => {
+        setNumberOfCategoryToShow((prev) => prev - 10);
+    };
 
-  const handleCategoryClick = (category) => {
-    setHotelCategory(category);
-  };
 
-  return (
-    <section className="categories d-flex align-center gap-large cursor-pointer">
-      {numberOfCategoryToShow >= 10 && (
-        <button
-          className="button btn-category btn-left fixed cursor-pointer"
-          onClick={handleShowMoreLeftClick}
-        >
-          <span class="material-icons-outlined">chevron_left</span>
-        </button>
-      )}
-      {categories &&
-        categories.map(({ _id, category }) => (
-          <span
-            className={`${category === hotelCategory ? "border-bottom" : ""}`}
-            key={_id}
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category}
-          </span>
-        ))}
-      {numberOfCategoryToShow - 10 < categories.length && (
-        <button
-          className="button btn-category btn-right fixed cursor-pointer"
-          onClick={handleShowMoreRightClick}
-        >
-          <span class="material-icons-outlined">chevron_right</span>
-        </button>
-      )}
-      <button
-        className="button btn-filter d-flex align-center gap-small cursor-pointer fixed"
-        onClick={handleFilterClick}
-      >
-        <span className="material-icons-outlined">filter_alt</span>
-        <span>Filter</span>
-      </button>
-    </section>
-  );
-};
+    const handleFilterClick = () => { 
+        filterDispatch({
+            type: "SHOW_FILTER_MODAL",
+           
+        })
+    }
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await axios.get(
+                    "https://travel-app-vm2w.onrender.com/api/category"
+                );
+                const categoriesToShow = data.slice(numberOfCategoryToShow + 10 > data.length ? data.length - 10 : numberOfCategoryToShow , numberOfCategoryToShow > data.length ? data.length : numberOfCategoryToShow + 10);
+                setCategories(categoriesToShow);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+        )()
+    }, [numberOfCategoryToShow]);
+
+    const handleCategoryClick = ( category) => {
+
+          setHotelCategory(category);
+    };
+    
+  
+
+
+    return (
+        <section className="categories d-flex aline center gap-large cursor-pointer ">
+
+            {
+                numberOfCategoryToShow >= 10 && (
+                    <button className="button btn-category btn-left fixed cursor-pointer" onClick={handleShowMoreLeftClick}>
+                        <span className="material-icons-outlined">chevron_left</span>
+                    </button>)
+            }
+
+            {
+                categories && categories.map(({ _id, category }) => (
+                    <span className={`${category === hotelCategory ? "border-bottom" : ""}`} key={_id} onClick={() => handleCategoryClick (category)}>
+                    {category}
+                    </span>
+                ))}
+            {
+                numberOfCategoryToShow - 10  < categories.length && (
+                    <button className="button btn-category btn-right fixed cursor-pointer" onClick={handleShowMoreRightClick}>
+                    <span className="material-icons-outlined">chevron_right</span>
+                </button>)
+            }
+            <button className="button btn-filter d-flex align-center gap-small cursor-pointer fixed" onClick={handleFilterClick}>
+            <span className="material-icons-outlined">filter_list</span>
+            <span className="filter-text">Filter</span>
+               
+            </button>
+        </section>
+    )
+
+}
